a26ecfa53e33a94ce5080382bfa91d090dd98418
B4KO <69869288+B4KO@users.noreply.github.com>
2024-10-16 21:25:52 +0200

    Fixed cards and pictures

--- src/Components/TeamSection.jsx
+++ src/Components/TeamSection.jsx
@@ -6,24 +6,25 @@ const TeamSection = ({ teamMembers }) => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
-        <motion.div
-          className="flex flex-wrap justify-center items-center space-x-4 space-y-4"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: true }}
-        >
-          {teamMembers.map((member, index) => (
-            <Card
-              key={index}
-              imageUrl={member.imageUrl}
-              title={member.title}
-              description={member.description}
-              linkedinUrl={member.linkedinUrl}
-              githubUrl={member.githubUrl}
-            />
-          ))}
-        </motion.div>
+      <motion.div
+  className="flex flex-wrap justify-center items-stretch gap-4"
+  initial={{ opacity: 0 }}
+  whileInView={{ opacity: 1 }}
+  transition={{ duration: 1.5 }}
+  viewport={{ once: true }}
+>
+  {teamMembers.map((member, index) => (
+    <Card
+      key={index}
+      imageUrl={member.imageUrl}
+      title={member.title}
+      description={member.description}
+      linkedinUrl={member.linkedinUrl}
+      githubUrl={member.githubUrl}
+    />
+  ))}
+</motion.div>
+
       </div>
     </div>
   );
